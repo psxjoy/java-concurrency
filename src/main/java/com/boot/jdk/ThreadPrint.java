d89b2308097de24bffacff693d14b2d8d26eeb7a
Black Pan <psxjoy@outlook.com>
2024-07-01 23:13:54 +0800

    day-1

---
+++ src/main/java/com/boot/jdk/ThreadPrint.java
@@ -0,0 +1,27 @@
+package com.boot.jdk;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
+public class ThreadPrint {
+    public static void main(String[] args)throws Exception {
+        // 获取JAVA 线程管理MXBean
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        // 不需要
+        ThreadInfo[] threadInfos = threadMXBean.dumpAllThreads(false, false);
+        for (ThreadInfo threadInfo : threadInfos) {
+            System.out.println("[" + threadInfo.getThreadId() + "]" + threadInfo.getThreadName());
+        }
+        Thread.sleep(1000000);
+    }
+    /**
+     [6]Monitor Ctrl-Break
+     [5]Attach Listener
+     [4]Signal Dispatcher
+     [3]Finalizer
+     [2]Reference Handler
+     [1]main
+
+     */
+}
